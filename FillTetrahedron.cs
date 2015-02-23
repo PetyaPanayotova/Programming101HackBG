b77fe731420e3f7fd60f4de20c4924757197c5f7
Petya Panayotova <p.v.panayotova@gmail.com>
2015-02-23 23:33:25 +0200

    Fill Tetrahedron by using function
    Finding the volume of tetrahedron by its edge in C#

---
+++ FillTetrahedron.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FillTetrahedron
+{
+    class FillTetrahedron
+    {
+        static double fill_tetrahedron(int num)
+        {
+            double ammount = 1d / 12d * Math.Sqrt(2) * Math.Pow((num / 10), 3);
+            return ammount;
+        }
+        static void Main(string[] args)
+        {
+            double ammount = fill_tetrahedron(100);
+            Console.WriteLine("The ammount of water that can be filled in a tetrahedron with 100 cm edge length is {0:0.00} litres.", ammount);
+        }
+    }
+}
